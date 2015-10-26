28d571b4bdc035f3fb0a8c9ffdd8b5c18a8dbc78
PhnxRbrn <pennstate5013@gmail.com>
2015-10-26 13:55:26 -0400

    Added Copyright, imports, and parse_url function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
-#[test]
-fn it_works() {
+// Copyright (c) 2015 Chris Palmer <pennstate5013@gmail.com>
+// Use of this source code is governed by the GPLv3 license that can be
+// found in the LICENSE file.
+
+extern crate syndication;
+extern crate url;
+
+use url::{Url, UrlParser};
+use syndication::Feed;
+
+pub fn parse_url() {
+
 }
