4178fcf19cefbab4f3e478975f23d112dde549e8
Chris Palmer <pennstate5013@gmail.com>
2016-03-17 08:45:43 -0400

    Add xml test
    Still need to finish making it work

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,3 +5,19 @@ mod rss;
 
 extern crate chrono;
 extern crate feed;
+
+use feed::{Feed, FeedBuilder};
+
+#[test]
+fn from_url() {
+    unimplemented!()
+}
+
+
+#[test]
+fn from_xml() {
+    let feed = FeedBuilder::new().channel_from_xml("../rsc/sample.xml").finalize();
+    let channel = feed.channel();
+    assert_eq!("The Linux Action Show! OGG".to_string(), channel.title());
+    assert_eq!("http://www.jupiterbroadcasting.com".to_string(), channel.link());
+}
