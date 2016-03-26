91584d73af5f11016e885fd14cbdf182b5d2fa08
Chris Palmer <pennstate5013@gmail.com>
2016-03-26 01:40:16 -0400

    Minor doc fixes

--- src/feedio/mod.rs
+++ src/feedio/mod.rs
@@ -30,7 +30,7 @@ impl FeedReader {
     /// ```
     /// use feed::feedio::FeedReader;
     ///
-    /// let feed_reader = FeedReader::new(Some("String".to_owned()));
+    /// let feed_reader = FeedReader::new("String");
     /// ```
     pub fn new(feed: &str) -> FeedReader {
         let feed_string = feed.to_owned();
@@ -389,7 +389,7 @@ impl FeedReader {
     /// ```
     /// use feed::feedio::FeedReader;
     ///
-    /// let feed_reader = FeedReader::new(Some("String".to_owned()));
+    /// let feed_reader = FeedReader::new("String");
     /// let channel = feed_reader.channel();
     /// ```
     pub fn channel(self) -> Channel {
