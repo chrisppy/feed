64a713a5f6ed1f8f845b3b3a51ecd48482333df5
Chris Palmer <pennstate5013@gmail.com>
2016-03-29 23:06:07 -0400

    more code for to xml

--- src/feedio/mod.rs
+++ src/feedio/mod.rs
@@ -463,6 +463,223 @@ impl FeedWriter {
         writer.write(End(Element::new(description_tag_str)))
               .expect(errors::tag_end_error(description_tag_str).as_str());
 
+        if channel.generator().is_some() {
+            let generator_tag_str = "generator";
+            let generator_tag = Element::new(description_tag_str);
+            writer.write(Start(generator_tag))
+                  .expect(errors::tag_start_error(generator_tag_str).as_str());
+            writer.write(Text(Element::new(channel.generator().unwrap().as_str())))
+                  .expect(errors::tag_text_error(generator_tag_str).as_str());
+            writer.write(End(Element::new(generator_tag_str)))
+                  .expect(errors::tag_end_error(generator_tag_str).as_str());
+        }
+
+        if channel.docs().is_some() {
+            let docs_tag_str = "docs";
+            let docs_tag = Element::new(description_tag_str);
+            writer.write(Start(docs_tag))
+                  .expect(errors::tag_start_error(docs_tag_str).as_str());
+            writer.write(Text(Element::new(channel.docs().unwrap().as_str())))
+                  .expect(errors::tag_text_error(docs_tag_str).as_str());
+            writer.write(End(Element::new(docs_tag_str)))
+                  .expect(errors::tag_end_error(docs_tag_str).as_str());
+        }
+
+        if channel.language().is_some() {
+            let language_tag_str = "language";
+            let language_tag = Element::new(description_tag_str);
+            writer.write(Start(language_tag))
+                  .expect(errors::tag_start_error(language_tag_str).as_str());
+            writer.write(Text(Element::new(channel.language().unwrap().as_str())))
+                  .expect(errors::tag_text_error(language_tag_str).as_str());
+            writer.write(End(Element::new(language_tag_str)))
+                  .expect(errors::tag_end_error(language_tag_str).as_str());
+        }
+
+        if channel.copyright().is_some() {
+            let copyright_tag_str = "copyright";
+            let copyright_tag = Element::new(description_tag_str);
+            writer.write(Start(copyright_tag))
+                  .expect(errors::tag_start_error(copyright_tag_str).as_str());
+            writer.write(Text(Element::new(channel.copyright().unwrap().as_str())))
+                  .expect(errors::tag_text_error(copyright_tag_str).as_str());
+            writer.write(End(Element::new(copyright_tag_str)))
+                  .expect(errors::tag_end_error(copyright_tag_str).as_str());
+        }
+
+        if channel.managing_editor().is_some() {
+            let managing_editor_tag_str = "managing_editor";
+            let managing_editor_tag = Element::new(description_tag_str);
+            writer.write(Start(managing_editor_tag))
+                  .expect(errors::tag_start_error(managing_editor_tag_str).as_str());
+            writer.write(Text(Element::new(channel.managing_editor().unwrap().as_str())))
+                  .expect(errors::tag_text_error(managing_editor_tag_str).as_str());
+            writer.write(End(Element::new(managing_editor_tag_str)))
+                  .expect(errors::tag_end_error(managing_editor_tag_str).as_str());
+        }
+
+        if channel.web_master().is_some() {
+            let web_master_tag_str = "web_master";
+            let web_master_tag = Element::new(description_tag_str);
+            writer.write(Start(web_master_tag))
+                  .expect(errors::tag_start_error(web_master_tag_str).as_str());
+            writer.write(Text(Element::new(channel.web_master().unwrap().as_str())))
+                  .expect(errors::tag_text_error(web_master_tag_str).as_str());
+            writer.write(End(Element::new(web_master_tag_str)))
+                  .expect(errors::tag_end_error(web_master_tag_str).as_str());
+        }
+
+        if channel.pub_date().is_some() {
+            let pub_date_tag_str = "pub_date";
+            let pub_date_tag = Element::new(description_tag_str);
+            writer.write(Start(pub_date_tag))
+                  .expect(errors::tag_start_error(pub_date_tag_str).as_str());
+            writer.write(Text(Element::new(channel.pub_date().unwrap().to_rfc2822())))
+                  .expect(errors::tag_text_error(pub_date_tag_str).as_str());
+            writer.write(End(Element::new(pub_date_tag_str)))
+                  .expect(errors::tag_end_error(pub_date_tag_str).as_str());
+        }
+
+        if channel.last_build_date().is_some() {
+            let last_build_date_tag_str = "last_build_date";
+            let last_build_date_tag = Element::new(description_tag_str);
+            writer.write(Start(last_build_date_tag))
+                  .expect(errors::tag_start_error(last_build_date_tag_str).as_str());
+            writer.write(Text(Element::new(channel.last_build_date().unwrap().to_rfc2822())))
+                  .expect(errors::tag_text_error(last_build_date_tag_str).as_str());
+            writer.write(End(Element::new(last_build_date_tag_str)))
+                  .expect(errors::tag_end_error(last_build_date_tag_str).as_str());
+        }
+
+        if channel.cloud().is_some() {
+            let cloud_tag_str = "cloud";
+            let mut cloud_tag = Element::new(cloud_tag_str);
+            cloud_tag.push_attribute(b"domain", channel.cloud().unwrap().domain().as_str());
+            cloud_tag.push_attribute(b"port",
+                                     channel.cloud().unwrap().port().to_string().as_str());
+            cloud_tag.push_attribute(b"path", channel.cloud().unwrap().path().as_str());
+            cloud_tag.push_attribute(b"registerProcedure",
+                                     channel.cloud().unwrap().register_procedure().as_str());
+            cloud_tag.push_attribute(b"protocol", channel.cloud().unwrap().protocol().as_str());
+            writer.write(Start(cloud_tag))
+                  .expect(errors::tag_start_error(cloud_tag_str).as_str());
+            writer.write(End(Element::new(cloud_tag_str)))
+                  .expect(errors::tag_end_error(cloud_tag_str).as_str());
+        }
+
+        if channel.ttl().is_some() {
+            let ttl_tag_str = "ttl";
+            let ttl_tag = Element::new(description_tag_str);
+            writer.write(Start(ttl_tag))
+                  .expect(errors::tag_start_error(ttl_tag_str).as_str());
+            writer.write(Text(Element::new(channel.ttl().unwrap().to_string().as_str())))
+                  .expect(errors::tag_text_error(ttl_tag_str).as_str());
+            writer.write(End(Element::new(ttl_tag_str)))
+                  .expect(errors::tag_end_error(ttl_tag_str).as_str());
+        }
+
+        if channel.categories().is_some() {
+            for category in channel.categories().unwrap() {
+                let category_tag_str = "category";
+                let mut category_tag = Element::new(category_tag_str);
+                if category.domain().is_some() {
+                    category_tag.push_attribute(b"domain", category.domain().unwrap().as_str());
+                }
+                writer.write(Start(category_tag))
+                      .expect(errors::tag_start_error(category_tag_str).as_str());
+                writer.write(Text(Element::new(category.category())))
+                      .expect(errors::tag_text_error(category_tag_str).as_str());
+                writer.write(End(Element::new(category_tag_str)))
+                      .expect(errors::tag_end_error(category_tag_str).as_str());
+            }
+        }
+
+        if channel.image().is_some() {
+            let image_tag_str = "image";
+            let image_tag = Element::new(image_tag_str);
+            writer.write(Start(image_tag)).expect(errors::tag_start_error(image_tag_str).as_str());
+
+            let image_link_tag_str = "link";
+            let image_link_tag = Element::new(image_link_tag_str);
+            writer.write(Start(image_link_tag))
+                  .expect(errors::tag_start_error(image_link_tag_str).as_str());
+            writer.write(Text(Element::new(channel.image()
+                                                  .unwrap()
+                                                  .link()
+                                                  .as_str())))
+                  .expect(errors::tag_text_error(image_link_tag_str).as_str());
+            writer.write(End(Element::new(image_link_tag_str)))
+                  .expect(errors::tag_end_error(image_link_tag_str).as_str());
+
+            let image_url_tag_str = "url";
+            let image_url_tag = Element::new(image_url_tag_str);
+            writer.write(Start(image_url_tag))
+                  .expect(errors::tag_start_error(image_url_tag_str).as_str());
+            writer.write(Text(Element::new(channel.image()
+                                                  .unwrap()
+                                                  .url()
+                                                  .as_str())))
+                  .expect(errors::tag_text_error(image_url_tag_str).as_str());
+            writer.write(End(Element::new(image_url_tag_str)))
+                  .expect(errors::tag_end_error(image_url_tag_str).as_str());
+
+            let image_title_tag_str = "title";
+            let image_title_tag = Element::new(image_title_tag_str);
+            writer.write(Start(image_title_tag))
+                  .expect(errors::tag_start_error(image_title_tag_str).as_str());
+            writer.write(Text(Element::new(channel.image()
+                                                  .unwrap()
+                                                  .title()
+                                                  .as_str())))
+                  .expect(errors::tag_text_error(image_title_tag_str).as_str());
+            writer.write(End(Element::new(image_title_tag_str)))
+                  .expect(errors::tag_end_error(image_title_tag_str).as_str());
+
+            let image_width_tag_str = "width";
+            let image_width_tag = Element::new(image_width_tag_str);
+            writer.write(Start(image_width_tag))
+                  .expect(errors::tag_start_error(image_width_tag_str).as_str());
+            writer.write(Text(Element::new(channel.image()
+                                                  .unwrap()
+                                                  .width()
+                                                  .to_string()
+                                                  .as_str())))
+                  .expect(errors::tag_text_error(image_width_tag_str).as_str());
+            writer.write(End(Element::new(image_width_tag_str)))
+                  .expect(errors::tag_end_error(image_width_tag_str).as_str());
+
+            let image_height_tag_str = "height";
+            let image_height_tag = Element::new(image_height_tag_str);
+            writer.write(Start(image_height_tag))
+                  .expect(errors::tag_start_error(image_height_tag_str).as_str());
+            writer.write(Text(Element::new(channel.image()
+                                                  .unwrap()
+                                                  .height()
+                                                  .to_string()
+                                                  .as_str())))
+                  .expect(errors::tag_text_error(image_height_tag_str).as_str());
+            writer.write(End(Element::new(image_height_tag_str)))
+                  .expect(errors::tag_end_error(image_height_tag_str).as_str());
+
+            if channel.image().unwrap().description().is_some() {
+                let image_description_tag_str = "description";
+                let image_description_tag = Element::new(image_description_tag_str);
+                writer.write(Start(image_description_tag))
+                      .expect(errors::tag_start_error(image_description_tag_str).as_str());
+                writer.write(Text(Element::new(channel.image()
+                                                      .unwrap()
+                                                      .description()
+                                                      .unwrap()
+                                                      .as_str())))
+                      .expect(errors::tag_text_error(image_description_tag_str).as_str());
+                writer.write(End(Element::new(image_description_tag_str)))
+                      .expect(errors::tag_end_error(image_description_tag_str).as_str());
+            }
+
+            writer.write(End(Element::new(image_tag_str)))
+                  .expect(errors::tag_end_error(image_tag_str).as_str());
+        }
+
         writer.write(End(Element::new(channel_tag_str)))
               .expect(errors::tag_end_error(channel_tag_str).as_str());
         writer.write(End(Element::new(rss_tag_str)))
