ecf7590662435ae208996c2a181d8afda8979230
Chris Palmer <pennstate5013@gmail.com>
2017-04-07 23:21:15 -0400

    Add Validate Trait for Channel

--- src/channels/channel_validate.rs
+++ src/channels/channel_validate.rs
@@ -11,14 +11,90 @@
 //! `Validate` Trait for `Channel`
 
 
-use channels::Validate;
-use rss::Channel;
+use channels::{CategoryBuilder, CategoryGetters, ChannelBuilder, ChannelGetters, CloudBuilder, CloudGetters,
+               ImageBuilder, ImageGetters, Validate};
+use rss::{Category, Channel};
+use utils::string_utils;
 
 impl Validate for Channel
 {
     /// Validate `Channel`
-    fn validate() -> Result<Channel, String>
-    [
-        unimplemented!()
-    ]
+    fn validate(&self) -> Result<Channel, String>
+    {
+        let cloud = match self.cloud()
+        {
+            None => None,
+            Some(val) =>
+            {
+                Some(CloudBuilder::new().domain(val.domain().as_str())
+                         .port(string_utils::string_to_i64(val.port().as_str())?)
+                         .path(val.path().as_str())
+                         .register_procedure(val.register_procedure().as_str())
+                         .protocol(val.protocol().as_str())
+                         .validate()?
+                         .finalize()?)
+            }
+        };
+
+        let mut channel_cat: Vec<Category> = Vec::new();
+        for cat in self.categories()
+        {
+            channel_cat.push(CategoryBuilder::new().name(cat.name().as_str())
+                                 .domain(cat.domain())
+                                 .validate()?
+                                 .finalize()?);
+        }
+
+        let channel_cat_opt = if channel_cat.is_empty()
+        {
+            None
+        }
+        else
+        {
+            Some(channel_cat)
+        };
+
+
+        let image = match self.image()
+        {
+            None => None,
+            Some(val) =>
+            {
+                Some(ImageBuilder::new().url(val.url().as_str())
+                         .title(val.title().as_str())
+                         .link(val.link().as_str())
+                         .width(string_utils::option_string_to_option_i64(val.width())?)
+                         .height(string_utils::option_string_to_option_i64(val.height())?)
+                         .description(val.description())
+                         .validate()?
+                         .finalize()?)
+            }
+        };
+
+
+        ChannelBuilder::new()
+            .title(self.title().as_str())
+            .link(self.link().as_str())
+            .description(self.description().as_str())
+            .language(self.language())
+            .copyright(self.copyright())
+            .managing_editor(self.managing_editor())
+            .webmaster(self.webmaster())
+            .pub_date(self.pub_date())
+            .last_build_date(self.last_build_date())
+            .generator(self.generator())
+            .docs(self.docs())
+            .rating(None)
+            .ttl(string_utils::option_string_to_option_i64(self.ttl())?)
+            .cloud(cloud)
+            .categories(channel_cat_opt)
+            .image(image)
+            .text_input(None)
+            .skip_hours(None)
+            .skip_days(None)
+            .items(None)
+            .itunes_ext(None)
+            .validate()?
+            .finalize()
+    }
 }
--- src/utils/string_utils.rs
+++ src/utils/string_utils.rs
@@ -9,7 +9,36 @@
 
 use chrono::*;
 use std::i64;
+use std::str::FromStr;
 use url::Url;
+
+// Common code to convert String to i64
+pub fn string_to_i64(s: &str) -> Result<i64, String>
+{
+    match i64::from_str(s)
+    {
+        Ok(val) => Ok(val),
+        Err(err) => Err(format!("Error: {}", err)),
+    }
+}
+
+
+// Common code to convert Option<String> to Option<i64>
+pub fn option_string_to_option_i64(o: Option<String>) -> Result<Option<i64>, String>
+{
+    match o
+    {
+        Some(val) =>
+        {
+            match string_to_i64(val.as_str())
+            {
+                Ok(val) => Ok(Some(val)),
+                Err(err) => Err(err),
+            }
+        }
+        None => Ok(None),
+    }
+}
 
 
 // Common code to convert Option<String> from i64
